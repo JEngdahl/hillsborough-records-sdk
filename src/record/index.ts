c187fe914ee5980c97f162b1daec3defd32e787e
Jarett Engdahl <jaretten@gmail.com>
2020-12-23 12:31:58 -0500

    move logit into submit

--- src/record/index.ts
+++ src/record/index.ts
@@ -1,38 +1,45 @@
-import axios, { AxiosPromise } from 'axios';
+import axios, { AxiosPromise, AxiosResponse } from 'axios';
 import { RecordSearchType } from '../types/records';
 
 export class PublicRecord {
-  #baseUrl: string = 'https://pubrec6.hillsclerk.com/Public/ORIUtilities/OverlayWatermark/api/Watermark/';
-  records: RecordSearchType[] = [];
-  constructor(records: RecordSearchType | RecordSearchType[]) {
-    if (Array.isArray(records)) {
-      this.addMultiple(records);
-    } else {
-      this.addOne(records);
+    #baseUrl: string = 'https://pubrec6.hillsclerk.com/Public/ORIUtilities/OverlayWatermark/api/Watermark/';
+    records: RecordSearchType[] = [];
+    constructor(records?: RecordSearchType | RecordSearchType[]) {
+        if (Array.isArray(records)) {
+            this.addMultiple(records);
+        } else if (records !== undefined) {
+            this.addOne(records);
+        }
     }
-  }
 
-  addOne(record: RecordSearchType): PublicRecord {
-    this.records.push(record);
-    return this;
-  }
-
-  addMultiple(records: RecordSearchType[]): PublicRecord {
-    this.records = [...this.records, ...records];
-    return this;
-  }
-
-  private getRecordId(record: RecordSearchType) {
-    if (typeof record === 'string') {
-      return record;
+    addOne(record: RecordSearchType): PublicRecord {
+        this.records.push(record);
+        return this;
     }
-    return record.ID;
-  }
 
-  submit(): AxiosPromise[] {
-    return this.records
-      .flat()
-      .map(this.getRecordId)
-      .map((id) => axios.get(this.#baseUrl + id));
-  }
+    addMultiple(records: RecordSearchType[]): PublicRecord {
+        this.records = [...this.records, ...records];
+        return this;
+    }
+
+    private getRecordId(record: RecordSearchType) {
+        if (typeof record === 'string') {
+            return record;
+        }
+        return record.ID;
+    }
+
+    requestUrls(): string[] {
+        return this.records
+            .map(this.getRecordId)
+            .map(id => this.#baseUrl + id);
+    }
+
+    submit(): Promise<AxiosResponse[]> {
+        const reqs = this.requestUrls()
+            .map(url => axios.get(url));
+
+        return Promise.all(reqs)
+
+    }
 }
--- src/types/records.ts
+++ src/types/records.ts
@@ -1,17 +1,17 @@
 import { SearchableDocument } from './documents';
 
 export type PublicRecordType = {
-  Instrument: number;
-  PartiesOne: string[];
-  PartiesTwo: string[];
-  RecordDate: number;
-  DocType: SearchableDocument;
-  BookType: string;
-  BookNum: number;
-  PageNum: number;
-  Legal?: string;
-  SalesPrice: number;
-  ID: string;
+    Instrument: number;
+    PartiesOne: string[];
+    PartiesTwo: string[];
+    RecordDate: number;
+    DocType: SearchableDocument;
+    BookType: string;
+    BookNum: number;
+    PageNum: number;
+    Legal?: string;
+    SalesPrice: number;
+    ID: string;
 };
 
 export type RecordSearchType = string | PublicRecordType;
