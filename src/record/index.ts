1967cfcc60d1810494143120be03eb1280fd7b13
Jarett Engdahl <jaretten@gmail.com>
2020-12-23 12:40:56 -0500

    add promise catching

--- src/record/index.ts
+++ src/record/index.ts
@@ -2,40 +2,47 @@ import axios, { AxiosPromise, AxiosResponse } from 'axios';
 import { RecordSearchType } from '../types/records';
 
 export class PublicRecord {
-  #baseUrl: string = 'https://pubrec6.hillsclerk.com/Public/ORIUtilities/OverlayWatermark/api/Watermark/';
-  records: RecordSearchType[] = [];
-  constructor(records?: RecordSearchType | RecordSearchType[]) {
-    if (Array.isArray(records)) {
-      this.addMultiple(records);
-    } else if (records !== undefined) {
-      this.addOne(records);
+    #baseUrl: string = 'https://pubrec6.hillsclerk.com/Public/ORIUtilities/OverlayWatermark/api/Watermark/';
+    records: RecordSearchType[] = [];
+    constructor(records?: RecordSearchType | RecordSearchType[] | Promise<RecordSearchType[]>) {
+        if (records === undefined) {
+            return
+        }
+        Promise
+            .resolve(records)
+            .then(rs => {
+                if (Array.isArray(rs)) {
+                    this.addMultiple(rs);
+                } else if (rs !== undefined) {
+                    this.addOne(rs);
+                }
+            })
     }
-  }
 
-  addOne(record: RecordSearchType): PublicRecord {
-    this.records.push(record);
-    return this;
-  }
-
-  addMultiple(records: RecordSearchType[]): PublicRecord {
-    this.records = [...this.records, ...records];
-    return this;
-  }
-
-  private getRecordId(record: RecordSearchType) {
-    if (typeof record === 'string') {
-      return record;
+    addOne(record: RecordSearchType): PublicRecord {
+        this.records.push(record);
+        return this;
     }
-    return record.ID;
-  }
 
-  requestUrls(): string[] {
-    return this.records.map(this.getRecordId).map((id) => this.#baseUrl + id);
-  }
+    addMultiple(records: RecordSearchType[]): PublicRecord {
+        this.records = [...this.records, ...records];
+        return this;
+    }
 
-  submit(): Promise<AxiosResponse[]> {
-    const reqs = this.requestUrls().map((url) => axios.get(url));
+    private getRecordId(record: RecordSearchType) {
+        if (typeof record === 'string') {
+            return record;
+        }
+        return record.ID;
+    }
 
-    return Promise.all(reqs);
-  }
+    requestUrls(): string[] {
+        return this.records.map(this.getRecordId).map((id) => this.#baseUrl + id);
+    }
+
+    submit(): Promise<AxiosResponse[]> {
+        const reqs = this.requestUrls().map((url) => axios.get(url));
+
+        return Promise.all(reqs);
+    }
 }
