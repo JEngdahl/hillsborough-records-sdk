9f75bf5ef8583b87eed4282eb12d37d4057a50da
Jarett Engdahl <jaretten@gmail.com>
2020-12-23 13:59:11 -0500

    1.0.9

--- src/record/index.spec.ts
+++ src/record/index.spec.ts
@@ -1,54 +1,46 @@
-import { PublicRecord } from "./index"
-import { PublicRecordType } from "../types/records"
-import { documents } from "../types/documents"
+import { PublicRecord } from './index';
+import { PublicRecordType } from '../types/records';
+import { documents } from '../types/documents';
 
 const fakeDocFactory = (ID: string): PublicRecordType => ({
-    Instrument: 1,
-    PartiesOne: ["FAKE"],
-    PartiesTwo: ["FAKE"],
-    RecordDate: 1,
-    DocType: documents.MORTGAGE,
-    BookType: "0",
-    BookNum: 0,
-    PageNum: 0,
-    SalesPrice: 0,
-    ID
-})
+  Instrument: 1,
+  PartiesOne: ['FAKE'],
+  PartiesTwo: ['FAKE'],
+  RecordDate: 1,
+  DocType: documents.MORTGAGE,
+  BookType: '0',
+  BookNum: 0,
+  PageNum: 0,
+  SalesPrice: 0,
+  ID,
+});
 
-const recordPromiseFactory = (IDs: string[]): Promise<PublicRecordType[]> => new Promise(
-    (res, _) => res(
-        IDs.map(id => fakeDocFactory(id))
-    )
-)
+const recordPromiseFactory = (IDs: string[]): Promise<PublicRecordType[]> =>
+  new Promise((res, _) => res(IDs.map((id) => fakeDocFactory(id))));
 
-describe("Records", () => {
-    test("They should accept strings, and generate request urls", () => {
-        const result = new PublicRecord().addRecord("FAKE_ID")
-        expect(result.requestUrls().length).toBe(1)
-    })
-    test("They should accept strings, as well as DocRecords and generate request urls", () => {
-        const result = new PublicRecord().addRecord(fakeDocFactory("FAKE_ID"))
-        expect(result.requestUrls().length).toBe(1)
-    })
-    test("They should accept both and generate request urls", () => {
-        const result = new PublicRecord()
-            .addRecord(fakeDocFactory("FAKE_ID"))
-            .addRecord("FAKE_ID")
-        expect(result.requestUrls().length).toBe(2)
-    })
+describe('Records', () => {
+  test('They should accept strings, and generate request urls', () => {
+    const result = new PublicRecord().addRecord('FAKE_ID');
+    expect(result.requestUrls().length).toBe(1);
+  });
+  test('They should accept strings, as well as DocRecords and generate request urls', () => {
+    const result = new PublicRecord().addRecord(fakeDocFactory('FAKE_ID'));
+    expect(result.requestUrls().length).toBe(1);
+  });
+  test('They should accept both and generate request urls', () => {
+    const result = new PublicRecord().addRecord(fakeDocFactory('FAKE_ID')).addRecord('FAKE_ID');
+    expect(result.requestUrls().length).toBe(2);
+  });
 
-    test("They should accept both and generate request urls from adding multiple", () => {
-        const result = new PublicRecord()
-            .addMultiple([fakeDocFactory("FAKE_ID"), "FAKE_ID"])
-        expect(result.requestUrls().length).toBe(2)
-    })
+  test('They should accept both and generate request urls from adding multiple', () => {
+    const result = new PublicRecord().addMultiple([fakeDocFactory('FAKE_ID'), 'FAKE_ID']);
+    expect(result.requestUrls().length).toBe(2);
+  });
 
-    test("Can add a Promise to doc search", () => {
-        expect.assertions(1);
-        return new PublicRecord()
-            .addRecordPromise(
-                recordPromiseFactory(["FAKE_ID_1", "FAKE_ID_2", "FAKE_ID_3"]),
-            )
-            .then(rec => expect(rec.requestUrls().length).toBe(3))
-    })
-})
+  test('Can add a Promise to doc search', () => {
+    expect.assertions(1);
+    return new PublicRecord()
+      .addRecordPromise(recordPromiseFactory(['FAKE_ID_1', 'FAKE_ID_2', 'FAKE_ID_3']))
+      .then((rec) => expect(rec.requestUrls().length).toBe(3));
+  });
+});
--- src/record/index.ts
+++ src/record/index.ts
@@ -2,41 +2,41 @@ import axios, { AxiosPromise, AxiosResponse } from 'axios';
 import { RecordSearchType } from '../types/records';
 
 export class PublicRecord {
-    #baseUrl: string = 'https://pubrec6.hillsclerk.com/Public/ORIUtilities/OverlayWatermark/api/Watermark/';
-    records: RecordSearchType[] = [];
+  #baseUrl: string = 'https://pubrec6.hillsclerk.com/Public/ORIUtilities/OverlayWatermark/api/Watermark/';
+  records: RecordSearchType[] = [];
 
-    addRecord(record: RecordSearchType): PublicRecord {
-        this.records.push(record);
-        return this;
-    }
+  addRecord(record: RecordSearchType): PublicRecord {
+    this.records.push(record);
+    return this;
+  }
 
-    addMultiple(records: RecordSearchType[]): PublicRecord {
-        this.records = [...this.records, ...records]
-        return this;
-    }
+  addMultiple(records: RecordSearchType[]): PublicRecord {
+    this.records = [...this.records, ...records];
+    return this;
+  }
 
-    addRecordPromise(records: Promise<RecordSearchType[]>): Promise<PublicRecord> {
-        return Promise.resolve(records)
-            .then((rs) => {
-                this.records = [...this.records, ...rs]
-            })
-            .then(_ => this)
-        // return this;
-    }
+  addRecordPromise(records: Promise<RecordSearchType[]>): Promise<PublicRecord> {
+    return Promise.resolve(records)
+      .then((rs) => {
+        this.records = [...this.records, ...rs];
+      })
+      .then((_) => this);
+    // return this;
+  }
 
-    private getRecordId(record: RecordSearchType): string {
-        if (typeof record === 'string') {
-            return record;
-        }
-        return record.ID;
+  private getRecordId(record: RecordSearchType): string {
+    if (typeof record === 'string') {
+      return record;
     }
+    return record.ID;
+  }
 
-    requestUrls(): string[] {
-        return this.records.map(this.getRecordId).map((id) => this.#baseUrl + id);
-    }
+  requestUrls(): string[] {
+    return this.records.map(this.getRecordId).map((id) => this.#baseUrl + id);
+  }
 
-    submit(): Promise<AxiosResponse[]> {
-        const reqs = this.requestUrls().map((url) => axios.get(url));
-        return Promise.all(reqs);
-    }
+  submit(): Promise<AxiosResponse[]> {
+    const reqs = this.requestUrls().map((url) => axios.get(url));
+    return Promise.all(reqs);
+  }
 }
