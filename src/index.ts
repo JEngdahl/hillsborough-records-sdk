80c2362020efd396d3656021280010a58f60ae91
Jarett Engdahl <jaretten@gmail.com>
2020-12-23 11:44:14 -0500

    exports

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 export * from "./types/documents"
 export * from "./types/search"
+export * from "./types/records"
 
 export { Search } from "./search"
+export { PublicRecord } from "./record"
 
 
 
--- src/record/index.ts
+++ src/record/index.ts
@@ -1,23 +1,7 @@
-fetch("https://pubrec6.hillsclerk.com/Public/ORIUtilities/OverlayWatermark/api/Watermark/Abn3eRg4JmxozdppFskQhxbqxn%C3%894yXjvLXR5oUVbGoA0NVwBTRL8LyOOmzE3IioVOqwPZKbcERSupeinYrSqlsE=", {
-    "headers": {
-        "accept": "*/*",
-        "accept-language": "en-US,en;q=0.9,es;q=0.8",
-        "sec-fetch-dest": "empty",
-        "sec-fetch-mode": "cors",
-        "sec-fetch-site": "same-origin"
-    },
-    "referrer": "https://pubrec6.hillsclerk.com/res/dep/mozilla-pdf/2.2.228/web/viewer.html?file=%2FPublic%2FORIUtilities%2FOverlayWatermark%2Fapi%2FWatermark%2FAbn3eRg4JmxozdppFskQhxbqxn%C3%894yXjvLXR5oUVbGoA0NVwBTRL8LyOOmzE3IioVOqwPZKbcERSupeinYrSqlsE%3D",
-    "referrerPolicy": "strict-origin-when-cross-origin",
-    "body": null,
-    "method": "GET",
-    "mode": "cors",
-    "credentials": "include"
-});
-
 import axios, { AxiosPromise } from "axios";
 import { RecordSearchType } from "../types/records";
 
-class PublicRecord {
+export class PublicRecord {
     #baseUrl: string = "https://pubrec6.hillsclerk.com/Public/ORIUtilities/OverlayWatermark/api/Watermark/"
     records: RecordSearchType[] = []
     constructor(records: RecordSearchType | RecordSearchType[]) {
